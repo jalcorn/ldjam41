3e86db35e532c9afb0e5629b9633963802696c97
Russell Bloomdale <356717+rbloomdale@users.noreply.github.com>
2018-04-20 23:11:25 -0700

    Tower and Health

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour {
+
+  public float delayToStart = 5.0f;
+  public float secondsBetweenAttack = 1.0f;
+  public int damagePerAttack = -1;
+  public float damageRadius = 2f;
+  CircleCollider2D trigger;
+  TowerAnimation attackAnimation;
+
+  List<Character> charactersInRange = new List<Character>();
+
+	// Use this for initialization
+	void Start () {
+    var rigidbody = gameObject.AddComponent<Rigidbody2D>();
+    rigidbody.gravityScale = 0f;
+    trigger = gameObject.AddComponent<CircleCollider2D>();
+    trigger.isTrigger = true;
+    trigger.radius = damageRadius;
+    InvokeRepeating("Attack", delayToStart, secondsBetweenAttack);
+    attackAnimation = gameObject.GetComponentInChildren<TowerAnimation>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+  void Attack () {
+    attackAnimation.Play();
+    foreach (Character character in charactersInRange) {
+      character.AdjustHitpoints(damagePerAttack);
+    }
+  }
+
+  private void OnTriggerEnter2D(Collider2D collision)
+  {
+    Character character = collision.gameObject.GetComponent<Character>();
+    if (character != null) {
+      charactersInRange.Add(character);
+    }
+  }
+
+  private void OnTriggerExit2D(Collider2D collision)
+  {
+    Character character = collision.gameObject.GetComponent<Character>();
+    if (character != null) {
+      charactersInRange.Remove(character);
+    }
+  }
+}
---
+++ Assets/Scripts/TowerAnimation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerAnimation : MonoBehaviour {
+
+  new SpriteRenderer renderer;
+
+	// Use this for initialization
+	void Start () {
+    renderer = GetComponent<SpriteRenderer>();
+    Hide();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+  public void Play() {
+    Show();
+    Invoke("Hide", 0.15f);
+  }
+
+  void Show() {
+    renderer.enabled = true;
+  }
+
+  void Hide() {
+    renderer.enabled = false;
+  }
+}
