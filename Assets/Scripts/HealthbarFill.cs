bc5a72a60f3b44133cd247da58f1c4f39676c204
nick <n.watson.k@gmail.com>
2018-04-22 16:17:25 -0700

    Added healthbar script, not hooked up yet

---
+++ Assets/Scripts/HealthbarFill.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthbarFill : MonoBehaviour {
+    
+    private float healthPercent = 1f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.gameObject.transform.localScale = new Vector3(healthPercent,1,1);
+    }
+}
