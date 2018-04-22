a5a79086f384e1c00d579e6a647cb875bd35fefc
nick <n.watson.k@gmail.com>
2018-04-21 18:30:36 -0700

    added z positioning

---
+++ Assets/Scripts/SpriteZAdjust.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteZAdjust : MonoBehaviour {
+
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 myPos = transform.position;
+        Vector3 targetPos = target.transform.position;
+
+        targetPos.z = myPos.y;
+
+        target.transform.position = targetPos;
+	}
+}
