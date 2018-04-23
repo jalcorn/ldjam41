503ac05dd7d97443e2118f3b4b110f00bc2c9858
nick <n.watson.k@gmail.com>
2018-04-23 12:58:27 -0700

    better title screen

---
+++ Assets/ButtonHighlighter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHighlighter : MonoBehaviour {
+
+    private SpriteRenderer sprite;
+
+    private void Start() {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMouseOver() {
+        sprite.color = Color.gray;
+    }
+    private void OnMouseExit() {
+        sprite.color = Color.white;
+    }
+}
