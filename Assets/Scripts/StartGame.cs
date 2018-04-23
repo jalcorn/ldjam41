09e8b92c406724be59f57003647657fe193d09f6
Josh Alcorn <jalcorns@gmail.com>
2018-04-23 15:00:39 -0700

    fix text sizing, add keyboard shortcuts to start game; updated RC1 with these changes

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -3,6 +3,16 @@ using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour {
 	void OnMouseDown() {
+		LoadFirstScene();
+	}
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return)) {
+			LoadFirstScene();
+		}
+	}
+
+	private void LoadFirstScene() {
 		SceneManager.LoadScene("level1");
 	}
 }
