c5925349d0d74dc763efb159f7d5512b31f2753d
Russell Bloomdale <356717+rbloomdale@users.noreply.github.com>
2018-04-23 10:12:59 -0700

    warning vision enabled

--- Assets/Scripts/EnemyVision.cs
+++ Assets/Scripts/EnemyVision.cs
@@ -1,23 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class EnemyVision : MonoBehaviour {
 
   public Color neutralColor;
-  public Color targetedColor;
+	public Color almostDetectedColor;
+  public Color detectedColor;
 
   public SpriteRenderer indicator;
-
-  bool targeted = false;
 
   public delegate void PlayerSighted(GameObject player);
 
   PlayerSighted playerSightedHandler;
 
+	Collider2D detectedTrigger;
+	Collider2D almostDetectedTrigger;
+	ContactFilter2D filter = new ContactFilter2D().NoFilter();
+
+	DetectedState state = DetectedState.neutral;
+
 	// Use this for initialization
 	void Start () {
     indicator.color = neutralColor;
+		Collider2D[] colliders = GetComponents<Collider2D>();
+		detectedTrigger = colliders[0];
+		almostDetectedTrigger = colliders[1];
 	}
 	
 	// Update is called once per frame
@@ -25,15 +34,40 @@ public class EnemyVision : MonoBehaviour {
 		
   }
 
-  private void OnTriggerEnter2D(Collider2D collision) {
-    if (collision.gameObject.CompareTag("Player")) {
-      playerSightedHandler(collision.gameObject);
-      indicator.color = targetedColor;
-      targeted = true;
+  private void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag("Player")) {
+			Collider2D[] colliders = new Collider2D[20];
+			other.OverlapCollider(filter, colliders);
+			if (state == DetectedState.neutral && Array.Find(colliders, trigger => trigger == almostDetectedTrigger) != null) {				
+				indicator.color = almostDetectedColor;
+				state = DetectedState.almost;
+			} else if (state != DetectedState.detected && Array.Find(colliders, trigger => trigger == detectedTrigger) != null) {
+				playerSightedHandler(other.gameObject);
+				indicator.color = detectedColor;
+				state = DetectedState.detected;
+			}
     }
   }
 
-  public void watchForPlayer(PlayerSighted handler) {
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag("Player")) {
+			Collider2D[] colliders = new Collider2D[10];
+			other.OverlapCollider(filter, colliders);
+			if (state == DetectedState.almost && Array.Find(colliders, trigger => trigger == almostDetectedTrigger) == null) {
+				indicator.color = neutralColor;
+				state = DetectedState.neutral;
+			}
+		}
+	}
+
+	public void watchForPlayer(PlayerSighted handler) {
     playerSightedHandler = handler;
   }
+
+	enum DetectedState {
+		neutral,
+		almost,
+		detected
+	}
 }
