75ea6cea40a07f56f46432a7a076dfdcb2df9c0d
nick <n.watson.k@gmail.com>
2018-04-23 12:10:15 -0700

    Changed tower shot sprite

--- Assets/Scripts/TurretTower.cs
+++ Assets/Scripts/TurretTower.cs
@@ -2,11 +2,12 @@
 
 public class TurretTower : Tower {
 	public GameObject bulletPrefab;
+    public GameObject shotSpawnPosition;
 
 	internal override void Attack() {
 		if (charactersInRange.Count > 0) {
 			EnemyHealth target = charactersInRange[0];
-			GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+			GameObject bullet = Instantiate(bulletPrefab, shotSpawnPosition.transform.position, transform.rotation);
 			Projectile projectile = bullet.GetComponent<Projectile>();
 			if (target != null) {
 				projectile.SetTarget(target.transform);
